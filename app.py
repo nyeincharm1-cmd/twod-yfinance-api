afa8178a00eb28b0a6bf68dd5163915b220a5114
nyeincharm1-cmd <nyeincharm1@gmail.com>
2026-07-06 03:06:03 +0630

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "success": True,
+        "message": "API running"
+    })
+
+@app.route("/today")
+def today():
+    return jsonify({
+        "success": True,
+        "date": "2026-07-06",
+        "set": "1234.56",
+        "value": "987654",
+        "morningResult": "56",
+        "eveningResult": "56"
+    })
+
+@app.route("/status")
+def status():
+    return jsonify({
+        "success": True,
+        "status": "OPEN",
+        "session": "morning",
+        "current": "test"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
